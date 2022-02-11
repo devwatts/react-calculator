38c013c6658bd2670624b0b9dad9561f675a23ce
Dev Watts <devwatts5@gmaiil.com>
2022-02-11 22:52:23 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 
 function App() {
+  const createDigits = () => {
+    const digits = [];
+    for(let i=1;i<10;i++){
+      digits.push(<button key={i}>{i}</button>)
+    }
+    return digits;
+  }
   return (
-    <div className="App">
-      <div>
-        <h1>Hello World</h1>
+    <div className="calculator">
+      <div className="display">
+        <span>(0)</span>
+        <div>0</div>
+        
+      </div>
+      <div className="operators">
+        <button>+</button>
+        <button>-</button>
+        <button>*</button>
+        <button>/</button>
+        <button>DEL</button>
+      </div>
+      <div className="numbers">
+      {createDigits()}
+      <button>0</button>
+      <button>.</button>
+      <button>=</button>
       </div>
     </div>
   );
