2bdc5934200b3e8c0e823bd1621cdab6a70fe698
Dev Watts <devwatts5@gmaiil.com>
2022-02-12 02:49:30 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
-
+import {useState} from 'react';
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+  const ops = ["/","+","-","*","."]
+
+  const deleteFromDisplay = () =>{
+    setCalc(calc.slice(0,-1))
+  }
+
+  const getResult = () => {
+
+  }
+
+  const updateCalc = (value) => {
+    if(ops.includes(value) && calc === "" ||
+    ops.includes(value) && ops.includes(calc.slice(-1))
+    ){
+      return;
+    }
+    setCalc(calc + value);
+  }
+
   const createDigits = () => {
     const digits = [];
     for(let i=1;i<10;i++){
-      digits.push(<button key={i}>{i}</button>)
+      digits.push(<button onClick={() => updateCalc(i.toString())} key={i}>{i}</button>)
     }
     return digits;
   }
   return (
     <div className="calculator">
       <div className="display">
-        <span>(0)</span>
-        <div>0</div>
+        <span>{result || ""}</span>
+        <div>{calc || "0"}</div>
         
       </div>
       <div className="operators">
-        <button>+</button>
-        <button>-</button>
-        <button>*</button>
-        <button>/</button>
-        <button>DEL</button>
+        <button onClick={() => updateCalc("+")}>+</button>
+        <button onClick={() => updateCalc("-")}>-</button>
+        <button onClick={() => updateCalc("*")}>*</button>
+        <button onClick={() => updateCalc("/")}>/</button>
+        <button onClick={() => deleteFromDisplay()}>DEL</button>
       </div>
       <div className="numbers">
       {createDigits()}
-      <button>0</button>
-      <button>.</button>
-      <button>=</button>
+      <button onClick={() => updateCalc("0")}>0</button>
+      <button onClick={() => updateCalc(".")}>.</button>
+      <button onClick={() => updateCalc("")}>=</button>
       </div>
     </div>
   );
